ef7bd0d3fc51368603fcd46fff2597421856b656
BillyPacheco <billy.pacheco@paymentevolution.com>
2025-07-08 05:29:18 -0500

    Add project files.

---
+++ biblioteca_musical_v2_no_Asistant/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace biblioteca_musical_v2_no_Asistant.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
